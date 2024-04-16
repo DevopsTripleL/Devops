60a7a7a5db185bd88cb98650b6ffefc6c7579ede
RemiLecas <remi.lecas@ecole-isitech.fr>
2024-04-16 12:10:20 +0200

    Init de l'eslint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
+
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  let i = 0;
+  function Count () {
+    i++;
+    setCount(i)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +20,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={Count}>Ajoute +1</button>
+        <div> {count} </div>
         <a
           className="App-link"
           href="https://reactjs.org"
