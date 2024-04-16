aa32c4354b04c3f05b275e44ee3138c8c376faac
TristanIsitech <tristan.labaume@ecole-isitech.fr>
2024-04-16 15:00:35 +0200

    feat(reset de l'app propre): reset de l'app propre

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
-import logo from './logo.svg'
 import './App.css'
-import React, { useState } from 'react'
+import React from 'react'
 
 function App () {
-  const [count, setCount] = useState(0)
-  let i = 0
-  function Count () {
-    i++
-    setCount(i)
-  }
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={Count}>Ajoute +1</button>
-        <div> {count} </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Bienvenue
     </div>
   )
 }
