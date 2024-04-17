ec60afcf9b020362b79eaf2318606021d38837d7
TristanIsitech <tristan.labaume@ecole-isitech.fr>
2024-04-17 14:03:13 +0200

    Merge

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import App from './App'
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import Success from './SuccessPage/Success'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
@@ -10,5 +13,11 @@ root.render(
   <React.StrictMode>
     <App />
     <SpeedInsights />
+    <Router>
+      <Routes>
+        <Route path="/*" element={<App />} />
+        <Route path="/success" element={<Success />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 )
