21644e75a53546382ba7db967c9a97e909c08be4
TristanIsitech <tristan.labaume@ecole-isitech.fr>
2024-04-17 13:22:21 +0200

    feat: premier test unitaire
    Ajout de test unitaire pour le fonctionnement du formulaire

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import './App.css'
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
+import Form from './FormPage/Form'
+import Success from './SuccessPage/Success'
 
-function App () {
-  const navigate = useNavigate();
-  const [name, setName] = useState('');
-  const [surname, setSurname] = useState('');
-
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    navigate('/success', { state: { name, surname } });
-  }
+function App() {
 
   return (
     <div className="App">
-      <form className="App-form" onSubmit={handleSubmit}>
-        <label htmlFor="name">Nom :</label>
-        <input id="name" type="text" onChange={e => setName(e.target.value)} required/>
-        <label htmlFor="surname">Prenom :</label>
-        <input id="surname" type="text" onChange={e => setSurname(e.target.value)} required/>
-        <button type="submit" >Valider</button>
-      </form>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<Form />} />
+          <Route path="/success" element={<Success />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
