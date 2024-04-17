e0f761abf7c9e1d7244b49f88dda80ecea57d2d8
TristanIsitech <tristan.labaume@ecole-isitech.fr>
2024-04-17 11:12:01 +0200

    feat: add formulaire et page post formulaire
    Ajout d'un formulaire et d'une page post formulaire pour effectuer les tests !

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css'
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function App () {
+  const navigate = useNavigate();
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    navigate('/success', { state: { name, surname } });
+  }
+
   return (
     <div className="App">
-      Giga prout :)
+      <form className="App-form" onSubmit={handleSubmit}>
+        <label htmlFor="name">Nom :</label>
+        <input id="name" type="text" onChange={e => setName(e.target.value)} required/>
+        <label htmlFor="surname">Prenom :</label>
+        <input id="surname" type="text" onChange={e => setSurname(e.target.value)} required/>
+        <button type="submit" >Valider</button>
+      </form>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import App from './App'
+import Success from './SuccessPage/Success'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/*" element={<App />} />
+        <Route path="/success" element={<Success />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 )
