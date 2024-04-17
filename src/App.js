d59e74a40bdac67d8c7dc2c89cf7e6b4b1d48b42
RemiLecas <remi.lecas@ecole-isitech.fr>
2024-04-17 11:04:10 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import './App.css'
 import React from 'react'
-import packageJson from '../package.json'
+
 
 function App () {
-  const version = packageJson.version;
-  const lastCommitRef = process.env.REACT_APP_LAST_COMMIT_REF || 'N/A';
+
 
   return (
     <div className="App">
       prout :)
-
-      <div>
-        {(process.env.NODE_ENV === 'production') &&
-          <div>
-            Version : {version}.{lastCommitRef}.prod
-          </div>
-        }
-
-        {(process.env.NODE_ENV === 'development') &&
-          <div>
-            Version : {version}.{lastCommitRef}.dev
-          </div>
-        }
-      </div>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import packageJson from '../package.json';
 
+const lastCommitRef = process.env.REACT_APP_LAST_COMMIT_REF || 'N/A';
 const root = ReactDOM.createRoot(document.getElementById('root'))
+const version = packageJson.version;
+
 root.render(
   <React.StrictMode>
     <App />
+    <div>
+      {(process.env.NODE_ENV === 'production') &&
+        <div>
+          Version : {version}.{lastCommitRef}.prod
+        </div>
+      }
+
+      {(process.env.NODE_ENV === 'development') &&
+        <div>
+          Version : {version}.{lastCommitRef}.dev
+        </div>
+      }
+    </div>
     <SpeedInsights />
   </React.StrictMode>
 )
