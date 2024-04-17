fed1ffaaff875d660da190367bf3a549f9b2d7b6
RemiLecas <remi.lecas@ecole-isitech.fr>
2024-04-17 10:30:23 +0200

    ci: ajout CI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css'
 import React from 'react'
+import packageJson from '../package.json'
 
 function App () {
+  const version = packageJson.version;
+
   return (
     <div className="App">
-      Giga prout :)
+      Giga prout :')
+
+      <div>
+        {(process.env.NODE_ENV === 'production') &&
+          <div>
+            Version : {version}.prod
+          </div>
+        }
+
+        {(process.env.NODE_ENV === 'development') &&
+          <div>
+            Version : {version}.dev
+          </div>
+        }
+        <div>
+          Last Commit: {process.env.LAST_COMMIT_REF}
+        </div>
+      </div>
     </div>
   )
 }
